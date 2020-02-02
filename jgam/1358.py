88da2ea93c24ab54f1da5b17d372096e6b381f7c
jgam <jgam@alumni.nd.edu>
2020-02-02 16:54:00 +0900

    Update 1358.py

--- jgam/1358.py
+++ jgam/1358.py
@@ -11,7 +11,7 @@ while True:
     except EOFError:
         break
 '''
-
+import math
 
 def solution(inputList):
     # W H X Y Player
@@ -25,17 +25,38 @@ def solution(inputList):
     numPlayers = inputList[4]
 
     def circleCheck(x, y):
+        #newly calculate its yaxis with given xaixs
+        #left circle
+        if x <= xaxis:
+            newYaxisFirst = math.sqrt((height/2)**2 - (abs(xaxis-x))**2)
+            #here two regions first and second
+            if y >= int(height/2):
+                #if first region
+                if y <= newYaxisFirst:
+                    return 1
+            else:
+                if y >= newYaxisFirst:
+                    return 1
+        
+        #right circle
+        if x >= (xaxis+width):
+            newYaxisSecond = math.sqrt((height/2)**2 - (abs(x-xaxis))**2)
+            #two regions
+            if y>= int(height/2):
+                if y <= newYaxisSecond and y >= (yaxis+):
+                    return 1
+            else:
+                if y <= newYaxisSecond and y >= yaxis:
+                    return 1
         return 0
 
     def squareCheck(x, y):
-        #global width
-        #global height
         if x >= xaxis and x <= (width+xaxis):
             if y >= yaxis and y <= (yaxis+height):
                 return 1
         return 0
     squareCheck(1, 2)
-    # for
+    # for loop num players and its indexes
 
 
 solution([20, 10, 5, 0, 3])
